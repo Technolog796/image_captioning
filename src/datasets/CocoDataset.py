f17de50b6a86a97ea34f30a16d06ccd58b5c35f6
Ilia Kuleshov <kul757.48@mail.ru>
2023-06-14 20:43:49 +0300

    Add model and dataset

---
+++ src/datasets/CocoDataset.py
@@ -0,0 +1,117 @@
+import torch
+from torch.utils.data import Dataset
+import torchvision.datasets as dset
+
+import cv2
+from PIL import Image
+
+from transformers import AutoTokenizer
+import open_clip
+
+from tqdm.auto import tqdm
+import json
+import sys
+
+import matplotlib.pyplot as plt
+
+
+def read_image(path, size=(256, 256)):
+    image = cv2.imread(path)
+    image = cv2.resize(image, size)
+    image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    return image
+
+
+class CocoDataset(Dataset):
+    def __init__(self, image_path="data/coco_dataset/train2014",
+                 ann_path="data/coco_dataset/annotations/captions_train2014.json",
+                 caption_path="data/coco_dataset/coco_train_trainslation.jsonl", data_type='train', coef_size=0.1,
+                 tokenizer_name=Config.decoder,
+                 prefix_length=20, normalize_prefix=False):
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+        _, _, self.preprocess = open_clip.create_model_and_transforms(Config.encoder, pretrained="laion400m_e32")
+        self.prefix_length = prefix_length
+        self.normalize_prefix = normalize_prefix
+
+        self.img_path = image_path
+        self.ann_path = ann_path
+        self.caption_path = caption_path
+        coco_ds = dset.CocoDetection(root=image_path,
+                                     annFile=ann_path)
+        with open(caption_path, 'r') as f:
+            q = list(f)[0]
+            captions = json.loads(q)
+
+        self.img_paths = []
+
+        self.query_tokens = []
+        self.answer_tokens = []
+        max_seq_len = 0
+
+        max_img = len(captions) * coef_size
+        for i, caption_s in tqdm(enumerate(captions), total=max_img):
+            for j in range(len(captions[caption_s])):
+                query = "Что изображено на данной картинке?"
+                answer = captions[caption_s][j]
+                self.img_paths.append(caption_s)
+                self.query_tokens.append(torch.tensor(self.tokenizer.encode(query), dtype=torch.int64))
+                self.answer_tokens.append(torch.tensor(self.tokenizer.encode(answer), dtype=torch.int64))
+                max_seq_len = max(max_seq_len, self.answer_tokens[-1].shape[0])
+            if i >= max_img:
+                break
+        print("Data size is %0d" % len(self.img_paths))
+
+        del captions
+        del coco_ds
+        sys.stdout.flush()
+        all_len = torch.tensor([len(self.answer_tokens[i]) for i in range(len(self))]).float()
+        self.max_seq_len = prefix_length
+        self.type = data_type
+
+    def pad_tokens(self, item: int):
+        query_tokens = self.query_tokens[item]
+        padding = self.max_seq_len - query_tokens.shape[0]
+        if padding > 0:
+            query_tokens = torch.cat((query_tokens, torch.zeros(padding, dtype=torch.int64) - 1))
+            self.query_tokens[item] = query_tokens
+        elif padding < 0:
+            query_tokens = query_tokens[:self.max_seq_len]
+            self.query_tokens[item] = query_tokens
+        query_mask = query_tokens.ge(0)
+        query_tokens[~query_mask] = 0
+        query_mask = query_mask.float()
+
+        answer_tokens = self.answer_tokens[item]
+        padding = self.max_seq_len - answer_tokens.shape[0]
+        if padding > 0:
+            answer_tokens = torch.cat((answer_tokens, torch.zeros(padding, dtype=torch.int64) - 1))
+            self.answer_tokens[item] = answer_tokens
+        elif padding < 0:
+            answer_tokens = answer_tokens[:self.max_seq_len]
+            self.answer_tokens[item] = answer_tokens
+        answer_mask = answer_tokens.ge(0)
+        answer_tokens[~answer_mask] = 0
+        answer_mask = answer_mask.float()
+
+        return query_tokens, query_mask, answer_tokens, answer_mask
+
+    def __len__(self) -> int:
+        return len(self.img_paths)
+
+    def get_image(self, item):
+        name = str(self.img_paths[item])
+        name = f"{self.img_path}/{name}"
+        image = read_image(path=name)
+        return image
+
+    def __getitem__(self, item):
+        image = self.get_image(item)
+        image = self.preprocess(image).unsqueeze(0)
+        query_tokens, query_mask, answer_tokens, answer_mask = self.pad_tokens(item)
+        return query_tokens, query_mask, answer_tokens, answer_mask, image[0], item
+
+    def show_image(self, item):
+        img = self.get_image(item)
+        text = self.tokenizer.decode(self.pad_tokens(item)[2])
+        plt.imshow(img)
+        print(text)
---
+++ src/models/Model.py
@@ -0,0 +1,301 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as nnf
+from torch.amp import autocast
+
+from einops import rearrange
+
+import open_clip
+
+from transformers import  GPT2LMHeadModel, AutoTokenizer
+
+from typing import Optional
+
+
+import math
+
+def exists(val):
+    return val is not None
+
+
+def default(val, d):
+    return val if exists(val) else d
+
+
+def stable_softmax(t, dim=-1):
+    t = t - t.amax(dim=dim, keepdim=True)
+    return t.softmax(dim=dim)
+
+
+def scaled_dot_product(q, k, v, mask=None):
+    d_k = q.size()[-1]
+    attn_logits = torch.matmul(q, k.transpose(-2, -1))
+    attn_logits = attn_logits / math.sqrt(d_k)
+    if mask is not None:
+        attn_logits = attn_logits.masked_fill(mask == 0, -9e15)
+    attention = nnf.softmax(attn_logits, dim=-1)
+    values = torch.matmul(attention, v)
+    return values, attention
+
+
+def expand_mask(mask):
+    assert mask.ndim > 2, "Mask must be at least 2-dimensional with seq_length x seq_length"
+    if mask.ndim == 3:
+        mask = mask.unsqueeze(1)
+    while mask.ndim < 4:
+        mask = mask.unsqueeze(0)
+    return mask
+
+
+class BidirectionalCrossAttention(nn.Module):
+    def __init__(
+            self,
+            *,
+            dim,
+            heads=8,
+            dim_head=64,
+            context_dim=None,
+            dropout=0.,
+            talking_heads=False,
+            prenorm=False,
+    ):
+        super().__init__()
+        context_dim = default(context_dim, dim)
+        self.norm = nn.LayerNorm(dim) if prenorm else nn.Identity()
+        self.context_norm = nn.LayerNorm(context_dim) if prenorm else nn.Identity()
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        inner_dim = dim_head * heads
+        self.dropout = nn.Dropout(dropout)
+        self.context_dropout = nn.Dropout(dropout)
+        self.to_qk = nn.Linear(dim, inner_dim, bias=False)
+        self.context_to_qk = nn.Linear(context_dim, inner_dim, bias=False)
+        self.to_v = nn.Linear(dim, inner_dim, bias=False)
+        self.context_to_v = nn.Linear(context_dim, inner_dim, bias=False)
+        self.to_out = nn.Linear(inner_dim, dim)
+        self.context_to_out = nn.Linear(inner_dim, context_dim)
+        self.talking_heads = nn.Conv2d(heads, heads, 1, bias=False) if talking_heads else nn.Identity()
+        self.context_talking_heads = nn.Conv2d(heads, heads, 1, bias=False) if talking_heads else nn.Identity()
+
+    def forward(
+            self,
+            x,
+            context,
+            mask=None,
+            context_mask=None,
+            return_attn=False,
+            rel_pos_bias=None
+    ):
+        b, i, j, h, device = x.shape[0], x.shape[-2], context.shape[-2], self.heads, x.device
+        x = self.norm(x)
+        context = self.context_norm(context)
+        qk, v = self.to_qk(x), self.to_v(x)
+        context_qk, context_v = self.context_to_qk(context), self.context_to_v(context)
+        qk, context_qk, v, context_v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=h),
+                                           (qk, context_qk, v, context_v))
+        sim = einsum('b h i d, b h j d -> b h i j', qk, context_qk) * self.scale
+        if exists(rel_pos_bias):
+            sim = sim + rel_pos_bias
+        if exists(mask) or exists(context_mask):
+            mask = default(mask, torch.ones((b, i), device=device, dtype=torch.bool))
+            context_mask = default(context_mask, torch.ones((b, j), device=device, dtype=torch.bool))
+            attn_mask = rearrange(mask, 'b i -> b 1 i 1') * rearrange(context_mask, 'b j -> b 1 1 j')
+            sim = sim.masked_fill(~attn_mask, -torch.finfo(sim.dtype).max)
+        attn = stable_softmax(sim, dim=-1)
+        context_attn = stable_softmax(sim, dim=-2)
+        attn = self.dropout(attn)
+        context_attn = self.context_dropout(context_attn)
+        attn = self.talking_heads(attn)
+        context_attn = self.context_talking_heads(context_attn)
+        out = einsum('b h i j, b h j d -> b h i d', attn, context_v)
+        context_out = einsum('b h j i, b h j d -> b h i d', context_attn, v)
+        out, context_out = map(lambda t: rearrange(t, 'b h n d -> b n (h d)'), (out, context_out))
+        out = self.to_out(out)
+        context_out = self.context_to_out(context_out)
+        if return_attn:
+            return out, context_out, attn, context_attn
+        return out, context_out
+
+
+class MultiheadAttention(nn.Module):
+
+    def __init__(self, input_dim, embed_dim, num_heads):
+        super().__init__()
+        assert embed_dim % num_heads == 0, "Embedding dimension must be 0 modulo number of heads."
+
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+        self.qkv_proj = nn.Linear(input_dim, 3 * embed_dim)
+        self.o_proj = nn.Linear(embed_dim, embed_dim)
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        nn.init.xavier_uniform_(self.qkv_proj.weight)
+        self.qkv_proj.bias.data.fill_(0)
+        nn.init.xavier_uniform_(self.o_proj.weight)
+        self.o_proj.bias.data.fill_(0)
+
+    def forward(self, x, mask=None, return_attention=False):
+        batch_size, seq_length, _ = x.size()
+        if mask is not None:
+            mask = expand_mask(mask)
+        qkv = self.qkv_proj(x)
+        qkv = qkv.reshape(batch_size, seq_length, self.num_heads, 3 * self.head_dim)
+        qkv = qkv.permute(0, 2, 1, 3)
+        q, k, v = qkv.chunk(3, dim=-1)
+        values, attention = scaled_dot_product(q, k, v, mask=mask)
+        values = values.permute(0, 2, 1, 3)
+        values = values.reshape(batch_size, seq_length, self.embed_dim)
+        o = self.o_proj(values)
+        if return_attention:
+            return o, attention
+        else:
+            return o
+
+
+class QFormerBlock(nn.Module):
+    def __init__(self, img_emb_size, text_emb_size, output_size, bias=True, act=nn.Tanh):
+        super(QFormerBlock, self).__init__()
+
+        self.attn = MultiheadAttention(text_emb_size, text_emb_size, 16)
+        self.cross_attn = BidirectionalCrossAttention(
+            dim=img_emb_size,
+            heads=16,
+            dim_head=1024,
+            context_dim=text_emb_size
+        )
+        self.text_mlp = nn.Sequential(
+            nn.Linear(text_emb_size, text_emb_size * 2),
+            act(),
+            nn.Linear(text_emb_size * 2, text_emb_size * 2),
+            act(),
+            nn.Linear(text_emb_size * 2, output_size)
+        )
+
+    @autocast()
+    def forward(self, img_emb: torch.Tensor, text_emb: torch.Tensor) -> tuple[Any, Any]:
+        text_emb = self.attn(text_emb)
+        img_emb, text_emb = self.cross_attn(img_emb.reshape(-1, 1, img_emb.shape[1]), text_emb)
+        text_emb = self.text_mlp(text_emb)
+        return img_emb, text_emb
+
+
+class MySequential(nn.Sequential):
+    def forward(self, *inp):
+        for module in self._modules.values():
+            inp = module(*inp)
+            if inp[0].shape[1] == 1:
+                inp = (inp[0][:, 0, :], inp[1])
+        return inp
+
+
+class QFormer(nn.Module):
+    def __init__(self, img_emb_size, text_emb_size, output_size, n_blocks=4, bias=True, act=nn.Tanh):
+        super(QFormer, self).__init__()
+
+        self.blocks = MySequential(
+            *[QFormerBlock(img_emb_size, text_emb_size, text_emb_size) for _ in range(n_blocks)],
+        )
+        self.res = nn.Sequential(
+            nn.Linear(img_emb_size + text_emb_size, output_size)
+        )
+
+    @autocast()
+    def forward(self, img_emb: torch.Tensor, text_emb: torch.Tensor) -> torch.Tensor:
+        img_emb, text_emb = self.blocks(img_emb, text_emb)
+        text_emb = text_emb.mean(axis=1)
+        res_emb = torch.cat((img_emb, text_emb), axis=1)
+        res_emb = self.res(res_emb)
+        return res_emb
+
+
+class MLP(nn.Module):
+    def __init__(self, input_shape, output_shape, act=nn.Tanh):
+        super(MLP, self).__init__()
+        self.seq = nn.Sequential(
+            nn.Linear(input_shape, input_shape * 2),
+            act(),
+            nn.Linear(input_shape * 2, output_shape)
+        )
+
+    @autocast()
+    def forward(self, x):
+        return self.seq(x)
+
+
+def freeze(
+        model,
+        freeze_emb=False,
+        freeze_ln=False,
+        freeze_attn=False,
+        freeze_ff=False,
+        freeze_other=False,
+):
+    for name, p in model.named_parameters():
+        name = name.lower()
+        if 'ln' in name or 'norm' in name:
+            p.requires_grad = not freeze_ln
+        elif 'embeddings' in name:
+            p.requires_grad = not freeze_emb
+        elif 'mlp' in name:
+            p.requires_grad = not freeze_ff
+        elif 'attn' in name:
+            p.requires_grad = not freeze_attn
+        else:
+            p.requires_grad = not freeze_other
+
+    return model
+
+
+class ClipCaptionModel(nn.Module):
+    def __init__(self, prefix_length: int, prefix_size: int = 640, dist_loss=nn.MSELoss()):
+        super(ClipCaptionModel, self).__init__()
+        self.prefix_length = prefix_length
+        self.clip_model, _, _ = open_clip.create_model_and_transforms(Config.encoder, pretrained="laion400m_e32")
+        self.tokenizer = AutoTokenizer.from_pretrained(Config.decoder)
+        self.gpt = GPT2LMHeadModel.from_pretrained(Config.decoder,
+                                                   eos_token_id=self.tokenizer.pad_token_id)
+        self.gpt_embedding_size = self.gpt.transformer.wte.weight.shape[1]
+        self.clip_project = QFormer(prefix_size, self.gpt_embedding_size,
+                                    self.gpt_embedding_size * prefix_length)
+        self.device = Config.device
+        self.dist_loss = dist_loss
+        self.mlp = MLP(self.gpt_embedding_size, self.gpt_embedding_size)
+
+        for p in self.gpt.parameters():
+            p.requires_grad = False
+        for p in self.clip_model.parameters():
+            p.requires_grad = False
+
+    def get_dummy_token(self, batch_size: int, device: torch.device) -> torch.Tensor:
+        return torch.zeros(batch_size, self.prefix_length, dtype=torch.int64, device=device)
+
+    @autocast()
+    def forward(self, query_tokens: torch.Tensor, query_mask: Optional[torch.Tensor],
+                answer_tokens: torch.Tensor, answer_mask: Optional[torch.Tensor], image):
+        embedding_text = self.gpt.transformer.wte(query_tokens)
+        image = self.clip_model.encode_image(image)
+        prefix_projections = self.clip_project(image.float(), embedding_text).view(-1, self.prefix_length,
+                                                                                   self.gpt_embedding_size)
+        prefix_projections = self.mlp(prefix_projections)
+        out = self.gpt(inputs_embeds=prefix_projections, labels=answer_tokens)
+        return out, prefix_projections
+
+    def generate(self, image, texts):
+        tokens = torch.tensor(self.tokenizer.batch_encode_plus(texts)['input_ids'], dtype=torch.int64).to(self.device)
+
+        embedding_text = self.gpt.transformer.wte(tokens)
+        image = self.clip_model.encode_image(image)
+        prefix_projections = self.clip_project(image.float(), embedding_text).view(-1, self.prefix_length,
+                                                                                   self.gpt_embedding_size)
+        prefix_projections = self.mlp(prefix_projections)
+        out = self.gpt.generate(
+            inputs_embeds=prefix_projections,
+            max_length=self.prefix_length,
+            no_repeat_ngram_size=3,
+            repetition_penalty=2.,
+        )
+        res = [self.tokenizer.decode(x) for x in out]
+        return res
